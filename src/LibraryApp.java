b4812407c3d7eea701c6db626d81fa0ff2ac5096
VKo232 <31933554+VKo232@users.noreply.github.com>
2018-03-23 12:52:33 -0400

    Update LibraryApp.java

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class LibraryApp {
     private static Library library = new Library();
 
     public static void main(String[] args) {
-        System.out.println(library);
+    	init();
+        System.out.println("Welcome");
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+        	System.out.println("1. List members");
+        	System.out.println("2. List books");
+        	System.out.println("3. Display mailing address");
+        	System.out.println("4. Quit");
+        	int input = Integer.parseInt(sc.nextLine());
+        	if(input == 1) {
+        		for(int i = 0; i < library.members.length; i++) {
+        			System.out.println( i +". "library.members[i].name + "     " +  library.members[i].getAge() + " years old        " + library.members[i].address.toString());
+        		}
+        	} else if(input == 2) {
+        		
+        	} else if(input == 3) {
+        		
+        	} else if(input == 4) {
+        		break;
+        	}
+        }
     }
 }
