3dc574279e99677c473b373d7edcdcad6bd839c7
JaronBharat <36167785+JaronBharat@users.noreply.github.com>
2018-03-23 12:57:56 -0400

    Update LibraryApp.java

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -28,4 +28,52 @@ public class LibraryApp {
         	}
         }
     }
+        
+
+    public static void init() {
+        Member[]members = new Member[3];
+        Address address = new Address();
+        Book[] books = new Book[3];
+
+        for(int i = 0; i < 3; i++) {
+            members[i] = new Member();
+        }
+        members[0].name = "Demetrius";
+        members[0].birthYear = "2001";
+        members[0].address = "Somewhere idk man";
+
+        members[1].name = "Victor";
+        members[1].birthYear = "2000";
+        members[1].address = "St. Roberts";
+
+        members[2].name = "Gallo";
+        members[2].birthYear = "1982";
+        members[2].address = "Aurora";
+
+        address.street = "green lane";
+        address.city = "toronto";
+        address.province = "ontario";
+        address.postalCode = "L3T4T1";
+
+        library.name = "The Cranium";
+        library.location = address;
+
+        books[0].title = "Teacher stalking 101";
+        books[0].author = "Jaron B.";
+        books[0].pages = "30";
+
+        books[1].title = "The Giving Tree";
+        books[1].author = "Shel Silverstein";
+        books[1].pages = "25";
+
+        books[2].title = "Book";
+        books[2].author = "Author Writer";
+        books[2].pages = "1";
+
+        for(int i = 0; i < 3; i++) {
+            library.add(members[i]);
+            library.add(books[i]);
+        }
+    }
+
 }
